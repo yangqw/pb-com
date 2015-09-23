ca909c0670f4b57377c2c49ce81e2f5c9033ecea
Yang Bo <yangbodotnet@gmail.com>
2015-09-23 11:07:47 +0800

    9000 => 9300

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: process.env.PORT || 9000
+        port: process.env.PORT || 9333
       },
       dev: {
         options: {
@@ -191,7 +191,7 @@ module.exports = function (grunt) {
         options: {
           nodeArgs: ['--debug-brk'],
           env: {
-            PORT: process.env.PORT || 9000
+            PORT: process.env.PORT || 9333
           },
           callback: function (nodemon) {
             nodemon.on('log', function (event) {
