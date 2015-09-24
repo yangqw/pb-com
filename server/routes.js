52ddddd5e9d7d68130ec3bbd39c349914053fcf9
zz_mac <zz@mac.com>
2015-09-24 13:54:14 +0800

    set stormpath related config manually

--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,15 @@ var spExpressSdk = require('stormpath-sdk-express');
 var errors = require('./components/errors');
 var path = require('path');
 
-var spMiddleware = spExpressSdk.createMiddleware();
+var localEnv = require('./config/local.env');
+var spConfig = {
+  appHref: localEnv.STORMPATH_APP_HREF,
+  apiKeyId: localEnv.STORMPATH_API_KEY_ID,
+  apiKeySecret: localEnv.STORMPATH_API_KEY_SECRET,
+  // http://stackoverflow.com/questions/25870199/angularjs-cross-origin-request-blockeId
+  allowedOrigins: ['http://localhost:9333']
+};
+var spMiddleware = spExpressSdk.createMiddleware(spConfig);
 module.exports = function(app) {
   // Attach route handlers to Express
   spMiddleware.attachDefaults(app);
