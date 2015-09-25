08ea940141c2a4404fb8819544febc836b1a1e02
zz_mac <zz@mac.com>
2015-09-25 12:32:46 +0800

    add authenticated callback, the problem is how to get access token

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('caregiversComApp')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope, $http, $cookies) {
     $scope.message = 'Hello';
+    $scope.$on('$authenticated', function(scope, response) {
+      var getAccessToken = function() {
+      };
+      // 
+      $http.defaults.headers.common.Authorization = 'Bearer ' + getAccessToken();
+    })
   });
