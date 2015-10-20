cb6e0f2a5283f5b202f26044c8beae004c2d97fe
Kevin Qiwei Yang <yangqw@shinetechchina.com>
2015-10-20 16:55:06 +0800

    Added spGroupHref to CareGiveEnv, Fixed group checking and updatig afer login successfully

--- client/families/app/config/config.js
+++ client/families/app/config/config.js
@@ -3,10 +3,18 @@
 angular.module('caregiversComApp')
   .config(function(STORMPATH_CONFIG){
     //console.log("********AppConfig********");
-    if (CareGiverEnv) {// Overwrite STORMPATH_CONFIG keys if found definition at GareGiverEnv
-      STORMPATH_CONFIG.ENDPOINT_PREFIX = CareGiverEnv.server.host;
-      angular.forEach(CareGiverEnv.server.accesspoint, function(value, key) {
-        STORMPATH_CONFIG[key] = value;
-      })
+    if (CareGiverEnv) {
+      // Overwrite STORMPATH_CONFIG keys if found definition at GareGiverEnv
+      if (CareGiverEnv.server){
+        if (CareGiverEnv.server.host) STORMPATH_CONFIG.ENDPOINT_PREFIX = CareGiverEnv.server.host;
+
+        if (CareGiverEnv.server.accesspoint)
+          angular.forEach(CareGiverEnv.server.accesspoint, function(value, key) {
+            STORMPATH_CONFIG[key] = value;
+          });
+      }
+
+      // Set strompath group belongs for current subdomain
+      if (!CareGiverEnv.spGroupHref) CareGiverEnv.spGroupHref = 'https://api.stormpath.com/v1/groups/BEPUSki86n0koXCSa1Yu5';
     }
   });
--- client/families/app/main/main.controller.js
+++ client/families/app/main/main.controller.js
@@ -3,14 +3,14 @@
 angular.module('caregiversComApp')
   .controller('MainCtrl', function ($scope, $http, $rootScope, $user) {
     //console.log('MainCrtl');
-    $scope.awesomeThings = [];
+
     $scope.currentContact = {"firstName":"", "email":""};
     if ($user && $user.currentUser && $user.currentUser.contactId){
 
       $http.get(CareGiverEnv.server.host + '/contacts/' + $user.currentUser.contactId)
-      .success(function(awesomeThings) {
-        if (awesomeThings && awesomeThings.content)
-          $scope.currentContact = awesomeThings.content;
+      .success(function(response) {
+        if (response && response.content)
+          $scope.currentContact = response.content;
       });
     }
   });
