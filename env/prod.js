567557e80afde07fa3c71cf58749754dbe7230d7
Gary Gao <gaogang_0311@163.com>
2015-10-22 14:45:42 +0800

    Updated env.js

--- env/prod.js
+++ env/prod.js
@@ -1,11 +1,14 @@
 window.CareGiverEnv = {
   "server": {
     "accesspoint": {
-      "USER_COLLECTION_URI" : '/api/users/register',
-      "EMAIL_VERIFICATION_ENDPOINT" : 'api/users/emailVerification',
+      //"USER_COLLECTION_URI" : '/api/users/register',
+      "REGISTER_URI" : '/api/users/register',
+      "EMAIL_VERIFICATION_ENDPOINT" : '/api/users/emailVerification',
       "AUTHENTICATION_ENDPOINT" : '/api/users/oauth/token',
       "CURRENT_USER_URI" : '/api/users/current',
       "DESTROY_SESSION_ENDPOINT" : '/api/users/logout',
+      "FORGOT_PASSWORD_ENDPOINT" : '/api/users/forget_password',
+      "CHANGE_PASSWORD_ENDPOINT" : '/api/users/reset_password',
       "FORM_CONTENT_TYPE" : 'application/json',
     },
     "host": "https://api-proxy.caregivers.com",
