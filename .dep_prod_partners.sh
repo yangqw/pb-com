c3f3af5e1a1a4a87a42600f16cd50dc333ac79b7
Yang Bo <yangbodotnet@gmail.com>
2015-11-04 09:37:53 +0800

    script to build & deploy partner production

--- .dep_prod_partners.sh
+++ .dep_prod_partners.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-# Test
-#aws s3 sync ./www s3://new-sync1
+if ! command -v grunt > /dev/null; then
+  printf 'Grunt is not installed.\n'
+fi
+
+if ! command -v aws > /dev/null; then
+  printf 'aws-cli is not installed.\n'
+fi
+
+# Build families.cg.com for staging
+grunt build:prod --site=partners
+
+# sync directory to Staging on S3
 aws s3 sync ./www/partners s3://partners.caregivers.com --acl public-read
