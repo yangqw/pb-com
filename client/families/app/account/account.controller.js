d53f720f9cc7a99f3cf9a565c79f905ce0a13135
Kevin Qiwei Yang <yangqw@shinetechchina.com>
2015-11-23 16:40:18 +0800

    Fixed account ctrl logic for strip and killbill biz; removed payment section from profile page;

--- client/families/app/account/account.controller.js
+++ client/families/app/account/account.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('caregiversComApp')
-  .controller('AccountCtrl', function ($scope, $http, $user, $state, $window, $timeout) {
+  .controller('AccountCtrl', function ($scope, $rootScope, $http, $user, $state, $window, $timeout, Stormpath, Killbill) {
     $scope.editingPayment = false;
-
+    $rootScope.acceptedMsg = '';
+    $rootScope.error = '';
     if (!$user || !$user.currentUser) {
       $state.go('logout');
       return;
@@ -16,4 +17,26 @@ angular.module('caregiversComApp')
         $(".ccjs-csc-help").click();
     };
 
+    $scope.onGotStripeToken = function(status, response){
+      if (response.error || !response.id) {
+        $rootScope.error = response.error.message;
+        $rootScope.acceptedMsg = '';
+      } else {
+        $rootScope.error = '';
+        $rootScope.acceptedMsg = 'Successfully get your stripe credit card token...';
+        if ($user && $user.currentUser) $user.currentUser.payment = response;
+
+        // and submit to killbill for payment method update with StripeToken;
+        $user.currentUser.stripeToken = response.id;
+        Stormpath.updateSpUser().then(function(){
+          console.log("Updated current user with stripeToken:" + $user.currentUser.stripeToken);
+          Killbill.mapStripToKillBill().then(function(){
+            $timeout(function(){
+              $('.alert').alert('close');
+              $('.card-reveal .card-title').click();
+            }, 3000);
+          });
+        });
+      }
+    };
   });
