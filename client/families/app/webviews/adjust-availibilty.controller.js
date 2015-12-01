de83054aa86b1e5bbda005ecb6ddab171c432245
zz_mac <zz@mac.com>
2015-12-01 14:40:42 +0800

    fix adjust availibilty page

--- client/families/app/webviews/adjust-availibilty.controller.js
+++ client/families/app/webviews/adjust-availibilty.controller.js
@@ -1,25 +1,58 @@
 (function() {
   'use strict';
+
   angular.module('caregiversComApp')
   .controller('AdjustAvailibiltyCtrl', AdjustAvailibiltyCtrl);
 
-  AdjustAvailibiltyCtrl.$inject = ['$scope'];
-  function AdjustAvailibiltyCtrl($scope) {
+  AdjustAvailibiltyCtrl.$inject = ['$scope', 'filterFilter'];
+
+  function AdjustAvailibiltyCtrl($scope, filterFilter) {
     var vm = this;
+
+    vm.hoursPerWeek = [
+      {desc: '20 or fewer hours', id: 0},
+      {desc: '21 - 34 hours', id: 1},
+      {desc: '35+ hours', id: 2},
+    ]
+
+    vm.hoursPerWeekDesc = vm.hoursPerWeek[0].desc
 
     vm.availibiltyInfo = {
       days: {
-        'mon': false,
-        'tue': false,
-        'wed': true,
-        'thu': false,
-        'fri': true,
-        'sat': false
+        'sun': {status: false, display: "s"},
+        'mon': {status: false, display: "m"},
+        'tue': {status: false, display: "t"},
+        'wed': {status: true, display: "w"},
+        'thu': {status: false, display: "t"},
+        'fri': {status: false, display: "f"},
+        'sat': {status: false, display: "s"}
       },
+      hoursPerWeek: 0
+    }
 
-      hours: {
+    $scope.$watch('', function() {
+      $('.hours').dropdown();
+    })
+
+
+    var setHoursPerWeekDesc = function(option) {
+      for (var i in vm.hoursPerWeek) {
+        if (vm.hoursPerWeek[i].id === option.id) {
+          vm.hoursPerWeekDesc = option.desc
+        }
       }
     }
 
+    var toggleDay = function(day) {
+      vm.availibiltyInfo.days[day].status = !vm.availibiltyInfo.days[day].status
+    }
+
+    vm.toggleDay = toggleDay;
+
+    vm.selectHoursPerWeek = function(option) {
+      vm.availibiltyInfo.hoursPerWeek = option.id
+      setHoursPerWeekDesc(option)
+    }
   }
+
 })()
