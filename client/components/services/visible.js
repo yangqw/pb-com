29fd3661b77a86a1034851c6cb0de0f0ad81fc53
zz_mac <zz@mac.com>
2015-12-22 15:37:55 +0800

    move visible setup config to visible service

--- client/components/services/visible.js
+++ client/components/services/visible.js
@@ -1,6 +1,21 @@
 (function(){
   angular.module('caregiversComApp')
   .factory('visibleService', visible)
+  .run(['$rootScope','visibleService',  function($rootScope, visibleService) {
+    $rootScope.$on('$stateChangeStart', function(e,toState,toParams){
+      var defaultVisibles = {
+        navbar: true,
+        sidebar: true,
+        footer: true
+      }
+      defaultVisibles = angular.extend(defaultVisibles, toState.visible || {})
+      // visibleService.visible = defaultVisibles;
+      visibleService.visible.navbar = defaultVisibles.navbar
+      visibleService.visible.sidebar = defaultVisibles.sidebar
+      visibleService.visible.footer = defaultVisibles.footer
+      // Event.broadcast('navbar.visible', {state: defaultVisibles.navbar})
+    })
+  }])
 
   visible.$inject = []
 
